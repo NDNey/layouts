cbd74c6b26ea90dcbc672ea7ea70e4b7866b8631
NDNey <ndavidney@gmail.com>
2021-06-29 14:25:54 -0400

    readme file5

--- practice.js
+++ practice.js
@@ -5,5 +5,23 @@ function averageLength(arr) {
    }function averageLength(arr) { 
     let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
    
-   return arr.map(e => e[0].repeat(average) )
+   return
+   
+   arr.map(e => e[0].repeat(average) )
    }
+
+   function averageLength(arr) { 
+    let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+   
+   return arr.map(e => e[0].repeat(average) )
+   }function averageLength(arr) { 
+    let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+   
+   return
+   
+   arr.map(e => e[0].repeat(average) )
+   }
+
+
+   
+
