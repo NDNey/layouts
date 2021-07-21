5159f42cc41ceb87ec131ff7471d8a3b2d118cc2
NDNey <ndavidney@gmail.com>
2021-07-21 14:51:51 -0400

    commit file practice8

--- practice.js
+++ practice.js
@@ -851,7 +851,49 @@ function averageLength(arr) {
         
         return
      
-         
+        return
+        function averageLength(arr) { 
+         let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+        
+        return arr.map(e => e[0].repeat(average) )
+        }function averageLength(arr) { 
+         let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+        
+        return
+        function averageLength(arr) { 
+         let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+        
+        return arr.map(e => e[0].repeat(average) )
+        }function averageLength(arr) { 
+         let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+        
+        return
+ 
+        function averageLength(arr) { 
+         let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+        
+        return arr.map(e => e[0].repeat(average) )
+        }function averageLength(arr) { 
+         let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+        
+        return
+        function averageLength(arr) { 
+         let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+        
+        return arr.map(e => e[0].repeat(average) )
+        }function averageLength(arr) { 
+         let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+        
+        returnfunction averageLength(arr) { 
+         let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+        
+        return arr.map(e => e[0].repeat(average) )
+        }function averageLength(arr) { 
+         let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+        
+        return
+     
+        
          
      
         
