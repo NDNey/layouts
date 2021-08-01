1077815187d0b04e2243ddbfe04734916bd655bc
NDNey <ndavidney@gmail.com>
2021-08-01 15:05:25 -0400

    commit file practice8

--- practice.js
+++ practice.js
@@ -2248,7 +2248,33 @@ function averageLength(arr) {
             return arr.map(e => e[0].repeat(average) )
             }function averageLength(arr) { 
              let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
-            
+             let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+         
+             return arr.map(e => e[0].repeat(average) )
+             }function averageLength(arr) { 
+              let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+             
+             return
+             function averageLength(arr) { 
+              let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+             
+             return arr.map(e => e[0].repeat(average) )
+             }function averageLength(arr) { 
+              let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+             
+             returnfunction averageLength(arr) { 
+              let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+             
+             return arr.map(e => e[0].repeat(average) )
+             }function averageLength(arr) { 
+              let average = Math.round(arr.reduce((a,b)=>a + b.length,0)/arr.length)
+             
+             return
+             
+             
+             
+             
+             
             return
             
             
